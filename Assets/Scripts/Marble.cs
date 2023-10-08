3cbcce8711ac0cbc88019848e66cbea26ef11029
sudoswiss <sudoswiss@gmail.com>
2023-10-08 22:29:25 +0200

    Add lateral movement

--- Assets/Scripts/Marble.cs
+++ Assets/Scripts/Marble.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Marble : MonoBehaviour
 {
     private GameInput gameInput;
+    private float lateralDistanceToMove = 1.5f;     // Total distance to move left and right
+    private float lateralDistanceToMovePerFrame = 0.01f;    // The amount to move in a single frame when moving left or right
 
     // Start is called before the first frame update
     void Start()
@@ -19,17 +21,39 @@ public class Marble : MonoBehaviour
 
     private void MoveRight_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
     {
-        
+        StartCoroutine(MoveRight());
     }
 
     private void MoveLeft_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
     {
-        
+        StartCoroutine(MoveLeft());
     }
 
     private void Jump_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
     {
         
+    }
+
+    IEnumerator MoveLeft()
+    {
+        for(float x = 0f; x > -lateralDistanceToMove; x-=lateralDistanceToMovePerFrame)
+        {
+            Debug.Log("Position.x: " + transform.position.x);
+            transform.position =
+                new Vector3(transform.position.x - lateralDistanceToMovePerFrame, transform.position.y, transform.position.z);
+            yield return null;
+        }
+    }
+
+    IEnumerator MoveRight()
+    {
+        for (float x = 0f; x < lateralDistanceToMove; x += lateralDistanceToMovePerFrame)
+        {
+            Debug.Log("Position.x: " + transform.position.x);
+            transform.position =
+                new Vector3(transform.position.x + lateralDistanceToMovePerFrame, transform.position.y, transform.position.z);
+            yield return null;
+        }
     }
 
     // Update is called once per frame
