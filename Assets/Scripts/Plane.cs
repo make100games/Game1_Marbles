2fa36cfe969d0a661de6154d349c36f51b32977d
sudoswiss <sudoswiss@gmail.com>
2023-10-22 15:14:10 +0200

    Basic flying physics. Way too rocket-thrusty though. Does not behave like a plane yet

--- Assets/Scripts/Plane.cs
+++ Assets/Scripts/Plane.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class Plane : MonoBehaviour
@@ -7,12 +8,16 @@ public class Plane : MonoBehaviour
     private GameInput gameInput;
     private float amountToRollInDegrees = 45;   // Amount of degrees to roll to the left or right when flying left/right
     private float rateOfRoll = 0.25f;   // Amount to roll in a single frame update
+    private Rigidbody rb;
+    private bool movingLeft = false;
+    private bool movingRight = false;
 
     // Start is called before the first frame update
     void Start()
     {
         gameInput = new GameInput();
         gameInput.Enable();
+        rb = GetComponent<Rigidbody>();
 
         gameInput.Game.StartMovingLeft.performed += StartMovingLeft_performed;
         gameInput.Game.StopMovingLeft.performed += StopMovingLeft_performed;
@@ -22,26 +27,30 @@ public class Plane : MonoBehaviour
 
     private void StopMovingRight_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
     {
-        Debug.Log("Stop moving right");
+        UnityEngine.Debug.Log("Stop moving right");
         StartCoroutine(RollToTheLeft());
+        movingRight = false;
     }
 
     private void StartMovingRight_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
     {
-        Debug.Log("Start moving right");
+        UnityEngine.Debug.Log("Start moving right");
         StartCoroutine(RollToTheRight());
+        movingRight = true;
     }
 
     private void StopMovingLeft_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
     {
-        Debug.Log("Stop moving left");
+        UnityEngine.Debug.Log("Stop moving left");
         StartCoroutine(RollToTheRight());
+        movingLeft = false;
     }
 
     private void StartMovingLeft_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
     {
-        Debug.Log("Start moving left");
+        UnityEngine.Debug.Log("Start moving left");
         StartCoroutine(RollToTheLeft());
+        movingLeft = true;
     }
 
     IEnumerator RollToTheLeft()
@@ -61,6 +70,25 @@ public class Plane : MonoBehaviour
             transform.Rotate(Vector3.forward, -rateOfRoll);
 
             yield return null;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(movingRight)
+        {
+            UnityEngine.Debug.Log("Move right");
+            rb.AddForce(Vector3.right * 5f, ForceMode.Acceleration);
+        }
+        if(movingLeft)
+        {
+            UnityEngine.Debug.Log("Move left");
+            rb.AddForce(Vector3.left * 5f, ForceMode.Acceleration);
+        }
+        if(!movingLeft && !movingRight)
+        {
+            // Decelerate depending if we are currently moving left or right
+            UnityEngine.Debug.Log("Rigid body velocity: " + rb.velocity);
         }
     }
 
