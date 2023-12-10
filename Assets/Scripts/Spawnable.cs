70c981bed5eaaad42165382784c2f3d79003a272
sudoswiss <sudoswiss@gmail.com>
2023-12-10 22:42:58 +0100

    Delete objects that have flown away so far that they never enter deletion zones again

--- Assets/Scripts/Spawnable.cs
+++ Assets/Scripts/Spawnable.cs
@@ -2,22 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-// FIXME: If an object is never going to enter deletion zone, it will stick around forever.
-//  An example of this is an obstacle which increases its orbit over time and eventually never crosses
-//  deletion zone again. To fix this: if an object is so old that it must have crossed through
-//  deletion zone at least 2 times already, delete it anyway. That means it is most likely out of sight of
-//  the player anyway (e.g. some obstacle that has been hurled out into space).
-// This object should be removed once it has lived a certain amount of time
 public class Spawnable : MonoBehaviour
 {
     public float timeToLive = 5f;  // Time until this obstacle is removed (in seconds)
+    private float maxTimeToLive = 30f;  // Time after which something gest deleted regardless of where it is
+    private float initialTime = 0;
+    private float timeLived = 0;
     private bool inDeletionZone = false;
 
     // Update is called once per frame
     void Update()
     {
-        timeToLive -= Time.deltaTime;
-        if (timeToLive <= 0 && inDeletionZone)
+        timeLived += Time.deltaTime;
+        if ((timeLived >= timeToLive && inDeletionZone) || timeLived >= maxTimeToLive)
         {
             Destroy(this.gameObject);
         }
